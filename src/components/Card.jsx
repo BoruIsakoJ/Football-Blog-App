66f414d4e9bb8ada2c9566a8bff2cfad19ea0904
Boru Isako <isco.jaldy@gmail.com>
2025-04-24 11:32:56 +0300

    Added Card component that will display the blog content

---
+++ src/components/Card.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Card({ post }) {
+  if (!post) return <div>Loading...</div>;
+
+  // Function to get the first 20 words of the content
+  const getFirst20Words = (content) => {
+    const words = content.split(' ');
+    return words.slice(0, 20).join(' ') + (words.length > 20 ? '...' : '');
+  };
+
+  return (
+    <div className="card shadow-sm rounded-2 mb-4 mx-auto" style={{ width: '20rem', height:"500px" }}>
+      <img
+        src={post.image}
+        alt={post.title}
+        className="card-img-top"
+        style={{ height: '200px', objectFit: 'cover' }}
+      />
+
+      <div className="card-body">
+        <h5 className="card-title fw-bold text-primary">{post.title}</h5>
+        <p className="card-text text-muted">{`By ${post.author} | ${post.date}`}</p>
+        <p className="card-text ">{getFirst20Words(post.content)}</p>
+        <Link to={`/posts/${post.id}`} className="btn btn-link text-decoration-none">Read More</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
